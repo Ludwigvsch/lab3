d7f38b3d7c15fcdd16064f8f477253f72bc54711
Ludwig von Schoenfeldt <lvonschoenfeldt@ucsd.edu>
2023-10-23 17:00:39 -0700

    fixing bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,21 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+//The two tests we wrote for you in ArrayTests.java pass, but the two implementations of reverse methods in ArrayExamples.java have bugs! Write more tests in ArrayTests.java to demonstrate that the two methods, reverseInPlace and reversed, have bugs, and identify the bugs.
+
+// Test for reverseinplace
+@Test
+public void testReversedInPlace1() {
+  int[] input1 = { 3, 4, 5 };
+  ArrayExamples.reverseInPlace(input1);
+  assertArrayEquals(new int[]{ 5, 4, 3 }, input1); 
+}
+
+@Test
+public void testReversed1() {
+  int[] input1 = { 3, 4, 5 };
+  assertArrayEquals(new int[]{ 5, 4, 3 }, ArrayExamples.reversed(input1));
+}
 }
